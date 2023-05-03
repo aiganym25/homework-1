3c1c523dfd6fda1e54d963cb6e910ae793e1eccc
Aiganym Ospan <ospan.aiganym@gmail.com>
2023-05-04 00:21:00 +0600

    imported tag props

--- hw8/src/components/Tag/Tag.tsx
+++ hw8/src/components/Tag/Tag.tsx
@@ -1,3 +1,5 @@
+import { TagProps } from "../../interfaces/TagProps";
+
 export const Tag = ({ text, selected, onClick }: TagProps) => (
   <div
     className={`tag ${text}-tag ${selected ? `${text}-selected` : ""}`}
